a98e78caee7abf13451a66f9af0c6fc54b6f6c30
YogyashriPatil <yogyashripatil20@gmail.com>
2025-08-01 08:44:36 +0530

    Completed Day 8 task

--- day-08/index.js
+++ day-08/index.js
@@ -1,11 +1,22 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
 
+dotenv.config()
 const app=express(); //initialize
 
-const port=3000; //run on port number eg. 3002,3000,5173,8000,8080
+const port=process.env.PORT ; //run on port number eg. 3002,3000,5173,8000,8080
+
 app.get("/",(req,res) => {
     res.send("Hello world ! ");
 });
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods:['GET','POST'],
+    credentials:true
+}))
+app.use(express.json())
+app.unsubscribe(express.urlencoded({extended:true}));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
