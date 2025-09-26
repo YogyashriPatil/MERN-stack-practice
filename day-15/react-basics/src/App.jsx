569953797f1ed2e788343985505e95bf2cd60b97
YogyashriPatil <yogyashripatil20@gmail.com>
2025-09-26 07:21:59 +0530

    Reduces re-rendering component using memo

--- day-15/react-basics/src/App.jsx
+++ day-15/react-basics/src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
+import React, { useState , Fragment} from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <div>
-      <HeaderWithButton />
-      {/* <Header title={title}></Header> */}
-      <Header title="Yogyashri2"></Header>
-    </div>
-  )
-}
-
-function HeaderWithButton(){
+  
   const [title, setTitle] = useState("My name is harkirat")
   function updateTitle(){
     setTitle("My name is " + Math.random());
@@ -20,14 +11,19 @@ function HeaderWithButton(){
     <div>
       <button onClick={updateTitle}>Update the title </button>
       <Header title={title}></Header>
+      <Header title="Yogyashri2"></Header>
+      <Header title="Yogyashri2"></Header>
+      <Header title="Yogyashri2"></Header>
+      <Header title="Yogyashri2"></Header>
+      <Header title="Yogyashri2"></Header>
     </div>
   )
 }
 
-function Header({title}){
+const Header = React.memo(function Header({title}){
     return <div>
       {title}
     </div>
-}
+})
 
 export default App
