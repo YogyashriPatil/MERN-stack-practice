6d03e42a93ec5cea5e35c8192cb69d62879c122c
YogyashriPatil <yogyashripatil20@gmail.com>
2025-09-26 07:10:02 +0530

    Create randome number generate dynamic

--- day-15/react-basics/src/App.jsx
+++ day-15/react-basics/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [title, setTitle] = useState("My name is harkirat")
+  function updateTitle(){
+    setTitle("My name is " + Math.random());
+  }
   return (
     <div>
       <button onClick={updateTitle}>Update the title </button>
-      <Header title="Yogyashri1"></Header>
+      <Header title={title}></Header>
       <Header title="Yogyashri2"></Header>
-
     </div>
   )
 }
