329ef2f9527c548440f47c9cfe19f8c858c20e7e
YogyashriPatil <yogyashripatil20@gmail.com>
2025-09-28 20:30:10 +0530

    Use of the hook

--- day-15/card-project/src/App.jsx
+++ day-15/card-project/src/App.jsx
@@ -1,28 +1,31 @@
-
-
+import { useEffect } from "react"
 
 function App() {
+  useEffect(function() {
+    alert("hello")
+  })
+
   return (
     <div>
-      <CardWrapper innerComponent={<TextComponent />} />
+      hi there
     </div>
   )
 }
 
-function TextComponent() {
-  return <div>
-    hi there
-  </div>
-}
+// // function TextComponent() {
+// //   return <div>
+// //     hi there
+// //   </div>
+// // }
 
-function CardWrapper({innerComponent}) {
-    // create a diiv whihc has a border (hint: the way to create a border is border )
-    // and inside the div, renders the prop
+// function CardWrapper({children}) {
+//     // create a diiv whihc has a border (hint: the way to create a border is border )
+//     // and inside the div, renders the prop
 
-    return <div style={{border: "2px solid black"}}>
-      {innerComponent}
-    </div>
-}
+//     return <div style={{border: "2px solid black"}}>
+//       {children}
+//     </div>
+// }
 
 export default App
  
