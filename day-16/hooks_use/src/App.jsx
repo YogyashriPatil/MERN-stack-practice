af48c7dd96da87c1a3cc7d68595e05c96ffaef13
YogyashriPatil <yogyashripatil20@gmail.com>
2025-09-29 07:16:48 +0530

    Make a todo using useEffect using axios

--- day-16/hooks_use/src/App.jsx
+++ day-16/hooks_use/src/App.jsx
@@ -4,26 +4,49 @@ import './App.css'
 
 function App() {
 
-  const [todos, setTodos] = useState([]);
+  // const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://sum-server.100xdevs.com/todos")
-      .then(function(response) {
-        setTodos(response.data.todos)
-      })
-  })
+  // useEffect(() => {
+  //   axios.get("https://sum-server.100xdevs.com/todos")
+  //     .then(function(response) {
+  //       setTodos(response.data.todos)
+  //     })
+  // },[])
 
   return (
     <>
-        {todos.map(todo => <Todo title={title} description={description} />)}
+      <GettodoId id={1} />
+        {/* {todos.map(todo => <Todo title={title} description={description} />)} */}
     </>
   )
 }
 
+function GettodoId({id})
+{
+  const [todo, setTodos] = useState(0);
+
+  useEffect(() => {
+     axios.get("https://sum-server.100xdevs.com/todos")
+      .then(function(response) {
+       setTodos(response.data.todos)
+    })
+  })
+  //your effect here
+  return <div>
+    <h1>
+      {todo.title}
+    </h1>
+    <h4>
+      {description}
+    </h4>
+    {/* <input type='number' placeholder='enter the todo id ' /> */}
+  </div>
+}
 function Todo({title,description})
 {
   return <div>
     <h1> {title} </h1>
+    <h3> {description} </h3>
   </div>
 }
 
