05342d8f1af098dc26589a4518b0b45ab17b7089
YogyashriPatil <yogyashripatil20@gmail.com>
2025-09-29 08:30:26 +0530

    Some changes

--- day-16/assignment2/src/App.jsx
+++ day-16/assignment2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,12 +7,24 @@ function App() {
   const [count, setCount] = useState(0)
   const [inputValue, setinputValue] = useState(0)
 
-  let co=0;
+  // useEffect(() => {
+  //   let co=0;
 
-  for(let i=1;i<inputValue ; i++)
-  {
-    co=co+i;
-  }
+  //   for(let i=1;i<inputValue ; i++)
+  //   {
+  //     co=co+i;
+  //   }
+  // },[inputValue])
+  // run when input value chnaged
+
+  const coun= useMemo(() => {
+    let finalco=0;
+
+    for(let i=1;i<inputValue ; i++)
+    {
+      dinalco=finalco+i;
+    }
+  },[inputValue])
 
   return (
     <div>
@@ -21,7 +33,7 @@ function App() {
         }} placeholder={'find sum from 1 to n'}></input>
         <br/>
         <br/>
-        Sum from 1 to {inputValue} is {co}
+        Sum from 1 to {inputValue} is {coun}
         <br/>
         <button onClick={() => setCount((count) => count + 1)}>
           counter {count}
