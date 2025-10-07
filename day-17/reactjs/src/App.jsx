63350aba234937ba73cbc841fe5ff40e803b6a3c
YogyashriPatil <yogyashripatil20@gmail.com>
2025-10-07 21:04:13 +0530

    Make the factorial count

--- day-17/reactjs/src/App.jsx
+++ day-17/reactjs/src/App.jsx
@@ -3,53 +3,48 @@ import './App.css'
 import axios from "axios";
 
 function App() {
-  const [selectedId,setselectedId] = useState(1);
+  const [counter,setCounter] = useState(0);
+  const [inputValue,setinputvalue] = useState(0)
+
+  let count=0;
+  for(let i=1;i<inputValue;i++)
+  {
+    count=count * 1;
+  }
   return (
       <div>
-        <button onClick={function (){
-          setselectedId(1);
-        }}>1</button>
-
-        <button onClick={function (){
-          setselectedId(2);
-        }}>2</button>
-
-        <button onClick={function () {
-          setselectedId(3);
-        }}>3</button>
-
-        <button onClick={function () {
-          setselectedId(4);
-        }}>4</button>
-
-        <button onClick={function () {
-          setselectedId(5);
-        }}>5</button>
-
-        <Todo id={selectedId} />
+         <input onchange={function (e) {
+          setinputvalue(e.target.value);
+         }} placeholder='Enter the number' />
+           
+           <br></br>
+          Sume from 1 to {inputValue} is {count}
+         <button onClick={() => {
+          setCounter(counter+1);
+          }}> Counter</button> 
       </div>    
   )
 }
 
-function Todo({id}){
-  const [todo,settodo] = useState({});
+// function Todo({id}){
+//   const [todo,settodo] = useState({});
 
-  useEffect( () => {
-    axios.get("https://sum-server.100xdevs.com/todo?id="+id)
-      .then(response => {
-        settodo(response.data.todo)
-      })
-  }, [id])
+//   useEffect( () => {
+//     axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+//       .then(response => {
+//         settodo(response.data.todo)
+//       })
+//   }, [id])
 
-  return <div>
-    id : {id}
-    <h1>
-      {todo.title}
-    </h1>
-    <h3> 
-      {todo.description}
-    </h3>
-  </div>
-}
+//   return <div>
+//     id : {id}
+//     <h1>
+//       {todo.title}
+//     </h1>
+//     <h3> 
+//       {todo.description}
+//     </h3>
+//   </div>
+// }
 
 export default App;
