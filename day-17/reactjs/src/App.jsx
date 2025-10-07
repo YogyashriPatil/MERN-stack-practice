06ef44651441566cca1ed070bc06a0130e300b82
YogyashriPatil <yogyashripatil20@gmail.com>
2025-10-07 20:41:58 +0530

    Created the todo search using id

--- day-17/reactjs/src/App.jsx
+++ day-17/reactjs/src/App.jsx
@@ -1,25 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css' 
+import axios from "axios";
 
 function App() {
-  const [todo, settodo] = useState({})
-
-
-  
+  const [selectedId,setselectedId] = useState(1);
   return (
       <div>
-          <input placeholder='enter the id : ' />
-          <h1> 
-            {todo.title}
-          </h1>
-          <h4>
-            {todo.description}
-          </h4>
+        <button onClick={function (){
+          setselectedId(1);
+        }}>1</button>
 
+        <button onClick={function (){
+          setselectedId(2);
+        }}>2</button>
+
+        <button onClick={function () {
+          setselectedId(3);
+        }}>3</button>
+
+        <button onClick={function () {
+          setselectedId(4);
+        }}>4</button>
+
+        <button onClick={function () {
+          setselectedId(5);
+        }}>5</button>
+
+        <Todo id={selectedId} />
       </div>    
   )
 }
 
-export default App
+function Todo({id}){
+  const [todo,settodo] = useState({});
+
+  useEffect( () => {
+    axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+      .then(response => {
+        settodo(response.data.todo)
+      })
+  }, [])
+
+  return <div>
+    <h1>
+      {todo.title}
+    </h1>
+    <h3> 
+      {todo.description}
+    </h3>
+  </div>
+}
+
+export default App;
