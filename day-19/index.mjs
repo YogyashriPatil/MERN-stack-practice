dd795fe5985953e779c5c893207799ba2cba1a37
YogyashriPatil <yogyashripatil20@gmail.com>
2025-10-17 18:44:26 +0530

    Use of the the chalk library

--- day-19/index.mjs
+++ day-19/index.mjs
@@ -6,6 +6,10 @@ console.log(chalk.blue("Hello, world   "));
 console.log(chalk.red.bold("Hello, world   "));
 
 
+// run using this command
+
+// node --experimental-modules index.mjs
+
 // function sum(a,b){
 //     return a+b;
 // }
