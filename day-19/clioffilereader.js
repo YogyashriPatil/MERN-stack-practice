fff065b3bc045b449b6548192e2b8a80381df260
YogyashriPatil <yogyashripatil20@gmail.com>
2025-10-17 19:54:00 +0530

    Create the cli for file

--- day-19/clioffilereader.js
+++ day-19/clioffilereader.js
@@ -1,49 +1,58 @@
-const fs=require('fs');
+// const fs=require('fs');
 
-function main(filename) {
-    console.log(process.argv)
-    fs.readFile(filename, "utf8",function (err,data){
-        let total = 0;
-        for(let i=0;i<data.length;i++)
-        {
-            // console.log(data[i])
-            if(data[i] === " "){
-                total++;
-            }
-        }
-        console.log(total+1)
-    })
-}
-main("a.txt")
-
-
-
-
-
-
-
-// const commander=require("commander");
-// const { count } = require("console");
-// const fs = require('fs');
-// const program=new commander.Command();
-
-// program.name('counter')
-//     .description('cli to do file based tasks')  
-//     .version('0.8.0');
-
-// program.command('count')
-//     .description('count the number of lines in a file')
-//     .argument('<file>','file to count')
-//     .action((file) => {
-//         fs.readFile(file,'utf-8', (err,data) => {
-//             if(err) {
-//                 console.log(err);
+// function main(filename) {
+//     console.log(process.argv)
+//     fs.readFile(filename, "utf8",function (err,data){
+//         let total = 0;
+//         for(let i=0;i<data.length;i++)
+//         {
+//             // console.log(data[i])
+//             if(data[i] === " "){
+//                 total++;
 //             }
-//             else {
-//                 const lines = data.split('\n').length;
-//                 console.log(`there are ${lines} lines in ${file}`)
-//             }
-//         })
+//         }
+//         console.log(total+1)
 //     })
+// }
+// main("a.txt")
 
-// program.parse();
+
+const {Command} =require("commander");
+const fs = require('fs');
+const program=new Command();
+
+program.name('counter')
+    .description('cli to do file based tasks')  
+    .version('0.8.0');
+
+program.command('count')
+    .description('count the number of lines in a file')
+    .argument('<file>','file to count')
+    .action((file) => {
+        fs.readFile(file,'utf-8', (err,data) => {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                const lines = data.split(' ').length;
+                console.log(`there are ${lines} lines in ${file}`)
+            }
+        })
+    })
+program.command('countline')
+    .description('count the number of lines in a file')
+    .argument('<file>','file to count')
+    .action((file) => {
+        fs.readFile(file,'utf-8', (err,data) => {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                const lines = data.split('\n').length;
+                console.log(`there are ${lines} lines in ${file}`)
+            }
+        })
+    })
+
+
+program.parse();
