72f2a2a31ef48538da5234acf72ac79372264df2
YogyashriPatil <yogyashripatil20@gmail.com>
2025-10-24 20:04:45 +0530

    make command line interface

--- day-19/clioffilereader.js
+++ day-19/clioffilereader.js
@@ -1,58 +1,58 @@
-// const fs=require('fs');
+const fs=require('fs');
 
-// function main(filename) {
-//     console.log(process.argv)
-//     fs.readFile(filename, "utf8",function (err,data){
-//         let total = 0;
-//         for(let i=0;i<data.length;i++)
-//         {
-//             // console.log(data[i])
-//             if(data[i] === " "){
-//                 total++;
+function main(filename) {
+    console.log(process.argv)
+    fs.readFile(filename, "utf8",function (err,data){
+        let total = 0;
+        for(let i=0;i<data.length;i++)
+        {
+            // console.log(data[i])
+            if(data[i] === " "){
+                total++;
+            }
+        }
+        console.log(total+1)
+    })
+}
+main(process.argv[2])
+
+
+// const {Command} =require("commander");
+// const fs = require('fs');
+// const program=new Command();
+
+// program.name('counter')
+//     .description('cli to do file based tasks')  
+//     .version('0.8.0');
+
+// program.command('count')
+//     .description('count the number of lines in a file')
+//     .argument('<file>','file to count')
+//     .action((file) => {
+//         fs.readFile(file,'utf-8', (err,data) => {
+//             if(err) {
+//                 console.log(err);
 //             }
-//         }
-//         console.log(total+1)
+//             else {
+//                 const lines = data.split(' ').length;
+//                 console.log(`there are ${lines} lines in ${file}`)
+//             }
+//         })
 //     })
-// }
-// main("a.txt")
+// program.command('countline')
+//     .description('count the number of lines in a file')
+//     .argument('<file>','file to count')
+//     .action((file) => {
+//         fs.readFile(file,'utf-8', (err,data) => {
+//             if(err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 const lines = data.split('\n').length;
+//                 console.log(`there are ${lines} lines in ${file}`)
+//             }
+//         })
+//     })
 
 
-const {Command} =require("commander");
-const fs = require('fs');
-const program=new Command();
-
-program.name('counter')
-    .description('cli to do file based tasks')  
-    .version('0.8.0');
-
-program.command('count')
-    .description('count the number of lines in a file')
-    .argument('<file>','file to count')
-    .action((file) => {
-        fs.readFile(file,'utf-8', (err,data) => {
-            if(err) {
-                console.log(err);
-            }
-            else {
-                const lines = data.split(' ').length;
-                console.log(`there are ${lines} lines in ${file}`)
-            }
-        })
-    })
-program.command('countline')
-    .description('count the number of lines in a file')
-    .argument('<file>','file to count')
-    .action((file) => {
-        fs.readFile(file,'utf-8', (err,data) => {
-            if(err) {
-                console.log(err);
-            }
-            else {
-                const lines = data.split('\n').length;
-                console.log(`there are ${lines} lines in ${file}`)
-            }
-        })
-    })
-
-
-program.parse();
+// program.parse();
