49c030feed42489d2b7022d950305f039a810cab
YogyashriPatil <yogyashripatil20@gmail.com>
2025-10-24 21:13:55 +0530

    Adding the parameters in http server:

--- day-21/index.js
+++ day-21/index.js
@@ -7,6 +7,14 @@ app.get("/", (req,res) => {
     res.send("<h1>Hello </h1>")
 })
 
+app.get("/asum/:a/:b", (req,res) => {
+    const a=parseInt(req.params.a);
+    const b=parseInt(req.params.b);
+    res.json({
+        answer: a+b 
+    })
+})
+
 
 app.get("/sum", (req,res) => {
     const a=parseInt(req.query.a);
