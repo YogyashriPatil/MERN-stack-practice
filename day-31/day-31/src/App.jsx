e0f3560a26db86d8a1c30c816f3eb1e7c2d50803
YogyashriPatil <yogyashripatil20@gmail.com>
2025-11-16 19:18:05 +0530

    Make the post component

--- day-31/day-31/src/App.jsx
+++ day-31/day-31/src/App.jsx
@@ -7,29 +7,49 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{background: "#dfe6e9", 
+      height:"100vh"}}>
+        <div style={{ display: "flex",justifyContent:"center"}}>
+          <div>
+            <div>
+              <PostComponent />
+              <br />
+            </div>
+            <div>
+              <PostComponent />
+              <br />
+            </div>
+            <div>
+              <PostComponent />
+              <br />
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
+const style = { width: 200, backgroundColor:"white",borderRadius:10, borderColor:"gray", borderWidth: 1, padding: 20 }
+
+function PostComponent(){
+  return <div style={style}>
+    <div style={{display:"flex"}}>
+      <img src={"https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"} 
+        style={{
+          width:60,
+          height:60,
+          borderRadius:60
+      }}/>
+      <div style={{fontSize:10, marginLeft:10}}>
+        <b>100xDevs</b>
+        <div>26M+ Followers</div>
+        <div>12m</div>
+      </div>
+    </div>      
+                                       
+    <div style={{fontSize:12}}>
+      what to know how to win big? 
+    </div>
+  </div>
+}
 export default App
