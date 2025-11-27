3e6dd17f5c0ea5c1b0f8975b25fa36feca6a57e1
YogyashriPatil <yogyashripatil20@gmail.com>
2025-11-27 09:03:48 +0530

    Learn about the key and list

--- day-34/Some-features/src/App.jsx
+++ day-34/Some-features/src/App.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return <div>
-      <Card>
-        <h2>Card Title</h2>
-        <p> This is some content inside the card. </p>
-      </Card>
 
-      <Card>
-        <h2>Another card</h2>
-        <p>This card has different content</p>
-      </Card>
-      
+  const todos =[ {
+    title:"Go to gym",
+    done:false
+  },
+  {
+    title:"eat food",
+    done:true
+  }];
+  const todosComponents = todos.map(todo => <Todo title={todo.title} done={todo.done} /> )
+  return <div>
+      {/* {todosComponents}       */}
+      <Todo key={1} title={"eat food"} done={false} />
+      <Todo key={2} title={"go to college"} done={true} />
     </div>
 }
 
-function Card ({Children}) {
+function Todo ({title, done}) {
   return <div style={{
       border: '1px solid #cc',
       borderRadius: '5px',
@@ -27,7 +30,7 @@ function Card ({Children}) {
       margin: '10px',
       boxShadow:'2px 2p 5pxrgba(0,0,0,0.1)',
     }}>
-      {Children}
+      {title} - {done ? "Done !": "Not done"}
   </div>
 }
 
