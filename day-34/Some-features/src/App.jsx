a919ecaa5ff9c8c7f0f69012afd174caf3754e3e
YogyashriPatil <yogyashripatil20@gmail.com>
2025-11-27 09:15:45 +0530

    Learn about the inline styling

--- day-34/Some-features/src/App.jsx
+++ day-34/Some-features/src/App.jsx
@@ -4,34 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-  const todos =[ {
-    title:"Go to gym",
-    done:false
-  },
-  {
-    title:"eat food",
-    done:true
-  }];
-  const todosComponents = todos.map(todo => <Todo title={todo.title} done={todo.done} /> )
-  return <div>
-      {/* {todosComponents}       */}
-      <Todo key={1} title={"eat food"} done={false} />
-      <Todo key={2} title={"go to college"} done={true} />
+    return <div>
+      <Mycomponent />
+      <Mycomponent />
     </div>
 }
-
-function Todo ({title, done}) {
-  return <div style={{
-      border: '1px solid #cc',
-      borderRadius: '5px',
-      borderBlockColor: 'yellow',
-      padding: '20px',
-      margin: '10px',
-      boxShadow:'2px 2p 5pxrgba(0,0,0,0.1)',
-    }}>
-      {title} - {done ? "Done !": "Not done"}
+const componentStyle= {
+  backgroundColor: 'red',
+  color:'white',
+  padding: 10,
+  borderRadius: '10px'
+}
+function Mycomponent()
+{
+  return <div style={componentStyle}>
+    hi there
   </div>
 }
-
 export default App
