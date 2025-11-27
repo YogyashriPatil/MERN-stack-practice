3d380dce348165809e8cf6ad9f7e277d4f4fc03f
YogyashriPatil <yogyashripatil20@gmail.com>
2025-11-27 08:51:33 +0530

    Learn about the children

--- day-34/Some-features/src/App.jsx
+++ day-34/Some-features/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return (
-    <>
+  return <div>
+      <Card>
+        <h2>Card Title</h2>
+        <p> This is some content inside the card. </p>
+      </Card>
+
+      <Card>
+        <h2>Another card</h2>
+        <p>This card has different content</p>
+      </Card>
       
-    </>
-  )
+    </div>
+}
+
+function Card ({Children}) {
+  return <div style={{
+      border: '1px solid #cc',
+      borderRadius: '5px',
+      borderBlockColor: 'yellow',
+      padding: '20px',
+      margin: '10px',
+      boxShadow:'2px 2p 5pxrgba(0,0,0,0.1)',
+    }}>
+      {Children}
+  </div>
 }
 
 export default App
