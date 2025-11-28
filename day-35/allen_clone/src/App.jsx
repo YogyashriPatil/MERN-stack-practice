707b1c53bfa70f26bc5b8bb5e4eb2ae3ec30b682
YogyashriPatil <yogyashripatil20@gmail.com>
2025-11-28 12:11:32 +0530

    USe the routing and the useNavigate() hook

--- day-35/allen_clone/src/App.jsx
+++ day-35/allen_clone/src/App.jsx
@@ -1,11 +1,52 @@
 import { useState } from 'react'
-
+import {BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom"
 import './App.css'
 
 function App() {
   return <div>
-      <h1>Hii there</h1>
+      <BrowserRouter>
+        <Link to='/'>
+          Allen 
+        </Link> | 
+        <Link to='/neet/online-coaching-class-11'>
+          Class 11
+        </Link> |  
+        <Link to='/neet/online-coaching-class-12'>
+          class 12
+        </Link>
+
+        <Routes>
+          <Route path='/neet/online-coaching-class-11' element={<Class11Program />} />
+          <Route path='/neet/online-coaching-class-12' element={<Class12Program />} />
+          <Route path='/' element={<Landing />} />
+
+        </Routes>
+      </BrowserRouter>
+
     </div>
+}
+
+function Landing(){
+  return <div>
+    welcome to allen
+  </div>
+}
+function Class11Program(){
+  return <div>
+    NEET programs for class 11th
+  </div>
+}
+
+function Class12Program(){
+  const navigate = useNavigate();
+
+  function redirectUser(){
+    navigate("/")
+  }
+  return <div>
+    NEET programs for class 12th <br />
+    <button onClick={redirectUser}>Go to home page</button>
+  </div>
 }
 
 export default App
