f7f3bf3b14ad1f1006c395e719ad867d60970359
YogyashriPatil <yogyashripatil20@gmail.com>
2025-11-29 18:19:59 +0530

    make the custome hook for fetch

--- day-37/example-context-api - Copy/src/App.jsx
+++ day-37/example-context-api - Copy/src/App.jsx
@@ -1,37 +1,11 @@
-import { useState, createContext, useCallback, useContext, useEffect } from 'react'
 import './App.css'
-
+import { usePostTitle } from './hooks/useFetch';
 // custome hook
-// function useCounter(){
-//   const [count, setcount] = useState(0);
-
-//   function increaseCount(){
-//     setcount(count+1)
-//     // setcount(c => c+1)
-//   }
-//   return {
-//     count: count,
-//     increaseCount: increaseCount
-//   }
-// }
-
 
 function App() {
-  const [post, setPost] = useState({})
-  async function getPosts() {
-    const response= await fetch("https://jsonplaceholder.typicode.com/posts/1");
-    console.log(response)
-    const json = await response.json();
-    setPost(json);
-  }
-
-  useEffect(() => {
-    getPosts();
-  },[])
-
+  const postTitle = usePostTitle();
   return <div>
-    {post.title}
-    
+    {postTitle}
   </div>
 }
 
