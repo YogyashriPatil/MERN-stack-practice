5525739bdfa8322432394cda3322c0d02938c190
YogyashriPatil <yogyashripatil20@gmail.com>
2025-11-29 18:07:44 +0530

    use the usefetch() hook

--- day-37/example-context-api - Copy/src/App.jsx
+++ day-37/example-context-api - Copy/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState, createContext, useCallback, useContext } from 'react'
+import { useState, createContext, useCallback, useContext, useEffect } from 'react'
 import './App.css'
 
 // custome hook
-function useCounter(){
-  const [count, setcount] = useState(0);
+// function useCounter(){
+//   const [count, setcount] = useState(0);
 
-  function increaseCount(){
-    setcount(count+1)
-    // setcount(c => c+1)
-  }
-  return {
-    count: count,
-    increaseCount: increaseCount
-  }
-}
+//   function increaseCount(){
+//     setcount(count+1)
+//     // setcount(c => c+1)
+//   }
+//   return {
+//     count: count,
+//     increaseCount: increaseCount
+//   }
+// }
+
+
 function App() {
+  const [post, setPost] = useState({})
+  async function getPosts() {
+    const response= await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    console.log(response)
+    const json = await response.json();
+    setPost(json);
+  }
+
+  useEffect(() => {
+    getPosts();
+  },[])
+
   return <div>
-    <Counter />
-    <Counter />
-    <Counter />
-    <Counter />
+    {post.title}
+    
   </div>
 }
 
-function Counter(){
-  const { count, increaseCount} = useCounter();
-  return <div>
-      {count}
-      <button onClick={increaseCount}>Increase {count}</button>
-  </div>
-}
+// function Counter(){
+//   const { count, increaseCount} = useCounter();
+//   return <div>
+//       {count}
+//       <button onClick={increaseCount}>Increase {count}</button>
+//   </div>
+//}
 export default App
