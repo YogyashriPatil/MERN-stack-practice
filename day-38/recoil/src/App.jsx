1a75825497b933facaf2726d5225448862290351
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-01 12:49:39 +0530

    Use the selector in recoil

--- day-38/recoil/src/App.jsx
+++ day-38/recoil/src/App.jsx
@@ -1,48 +1,46 @@
 import { memo, useEffect, useState } from 'react';
 import './App.css'
+import { counterAtom, evenSelector } from './store/atoms/counter';
 // custome hook
 // npm i recoil
 function App() {
   return <div>
+    <RecoilRoot>
+      <Buttons />
       <Counter />
-    </div>   
+      <IsEven />
+    </RecoilRoot>
+  </div>   
 }
-function Counter(){
-    const [Count, setCount] = useState(0)
-
-  useEffect(() => {
-    setInterval(() => setCount(c => c +1 ), 
-    10000)
-    // setCount(c => c +1 )
-  }, [Count])
-
-  return <div>
-    <CurrentCount />
-    <Increase />
-    <Decrease  />
-  </div>
-}
-const CurrentCount = memo(function () {
-  return <div>
-    1
-  </div>
-}
-)
-
-function Increase(){
-  
+function Buttons(){
+  const setCount = useSetRecoilState(counterAtom)
   function increase(){
+    setCount(c => c+1)
+  }
+
+  function decrease(){
+    setCount(c => c-1)
+
   }
   return <div>
     <button onClick={increase}>Increase</button>
-  </div>
-}
-function Decrease({setCount}){
-  function decrease(){
-  }
-  return <div>
     <button onClick={decrease}>Decrease</button>
+
   </div>
 }
+function Counter(){
+  const count = useRecoilValue(counterAtom)
+  return <div>
+    {count}
+  </div>
+}
+
+function IsEven(){
+  const even = useRecoilValue(evenSelector)
+  return <div>
+    { even ? "Even" : "Odd"}
+  </div>
+}
+
 
 export default App
--- day-38/recoil/src/store/atoms/counter.js
+++ day-38/recoil/src/store/atoms/counter.js
@@ -1,7 +1,15 @@
-import { useDeferredValue } from 'react';
-import { atom } from 'recoil';
+import { atom , selector} from 'recoil';
 
 export const counterAtom = atom({
     default:0 ,
     key : "counter"
+})
+
+export const evenSelector = selector({
+    key: "isEvenSelector",
+    get: function({get}) {
+        const currentCount = get(counterAtom);
+        const isEven = (currentCount % 2 == 0);
+        return isEven;
+    }
 })
