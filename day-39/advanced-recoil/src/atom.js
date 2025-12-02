490ca3fc8dc5ec2a8d347095f13b8d1b443bb329
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-02 18:20:58 +0530

    USe the asynchronous call

--- day-39/advanced-recoil/src/atom.js
+++ day-39/advanced-recoil/src/atom.js
@@ -1,13 +1,15 @@
+import axios from "axios";
 import { atom, selector } from "recoil";
 
 export const notifications = atom({
     key: "networkAtom",
-    default: {
-        network: 4, 
-        jobs: 6, 
-        messaging: 3, 
-        notifications: 3
-    }
+    default: selector({
+        key:"networkAtomSelector",
+        get : async () => {
+            const res = await axios.get("https://sum-server.100xdevs.com/notifications")
+            return res.data
+        }
+    })
 });
 
 export const totalNotificationSelector = selector({
