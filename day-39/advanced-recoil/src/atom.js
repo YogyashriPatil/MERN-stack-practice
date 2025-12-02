84302462956c5b809d19a93677d4b096dec7b805
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-02 19:54:45 +0530

    use the atomFamily

--- day-39/advanced-recoil/src/atom.js
+++ day-39/advanced-recoil/src/atom.js
@@ -1,6 +1,20 @@
 import axios from "axios";
 import { atom, selector } from "recoil";
 
+export const atomFamily = atomFamily({
+    key: "todosAtomFamily",
+    default: id => {
+        // map, filter
+        const foundTodo = null;
+        for(let i=0; i<TODOS.lenght; i++)
+        {
+            if(TODOS[i].id === id){
+                foundTodo = TODOS[i]
+            }
+        }
+        return foundTodo
+    }
+})
 export const notifications = atom({
     key: "networkAtom",
     default: selector({
