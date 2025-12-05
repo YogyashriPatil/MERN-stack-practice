2793d594fe84386beece176487a7e49af9e5bdd9
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-05 09:10:20 +0530

    Add the grid and the responsive sm,md

--- day-40/tailwind-practice/src/App.jsx
+++ day-40/tailwind-practice/src/App.jsx
@@ -5,9 +5,42 @@ function App() {
 
   return (
     <div>
+      FLEX DISPLAY
+      <br />
       <Test />
+
+      GRID DISPLAY
+      <br />
+      <GridLayout />
+
+      Responsive DISPLAY
+      <br />
+      <Responsive />
     </div>
   )
+}
+
+function Responsive(){
+  return <>
+    <div className='sm:bg-blue-300 bg-red-300'>
+      hi there
+    </div>
+  </>
+}
+function GridLayout(){
+    return  <>
+    <div class='grid grid-cols-12' >
+      <div className='bg-amber-500'>
+        Hi there 1
+      </div>
+      <div className='bg-red-500 col-span-6'>
+        Hi there 2
+      </div>
+      <div className='bg-green-500 col-span-2'>
+        Hi there 2
+      </div>
+    </div>
+  </>
 }
 function Test(){
   return  <>
