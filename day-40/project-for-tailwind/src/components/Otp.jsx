07f7f7ccc7890779cf923d4c2438560ae88884de
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-05 14:30:26 +0530

    Add the reference

--- day-40/project-for-tailwind/src/components/Otp.jsx
+++ day-40/project-for-tailwind/src/components/Otp.jsx
@@ -16,30 +16,56 @@ export function Otp(){
         }}/>
         <SubOtpBox reference={ref2} onDone={() => {
             ref3.current.focus();
+        }} goBack={() => {
+            ref1.current.focus();
         }} />
         <SubOtpBox reference={ref3} onDone={() => {
             ref4.current.focus();
-        }}/>
+        }} goBack={() => {
+            ref2.current.focus();
+        }} />
         <SubOtpBox reference={ref4} onDone={() => {
             ref5.current.focus();
-        }}/>
+        }} goBack={() => {
+            ref3.current.focus();
+        }} />
         <SubOtpBox reference={ref5} onDone={() => {
             ref6.current.focus();
+        }} goBack={() => {
+            ref4.current.focus();
         }} />
         <SubOtpBox reference={ref6} onDone={() => {
             setdisabled(false) 
+        }} goBack={() => {
+            ref5.current.focus();
         }} />
 
-        <br />
-        <br />
         <Button disabled={disabled} Children={"Verify"} ></Button>
     </div>
 }
 
-function SubOtpBox({reference, onDone}){
+function SubOtpBox({reference, onDone, goBack}){
+    const [inputBoxVal, setinputBoxval] = useState("")
     return <div>
-        <input ref={reference} onChange={(e) => {
-            onDone()
+        <input value={inputBoxVal} ref={reference} onKeyUp={(e) => {
+            if(e.key == "Backspace")
+            {
+                goBack()
+            }
+            console.log(e.key)
+        }} onChange={(e) => {
+            const val = e.target.value
+            if(val==""){
+                //go back logic
+               goBack()
+            }
+            else if( val=="1" || val=="2" ||val=="3" ||val=="4" ||val=="5" ||val=="6" ||val=="7" ||val=="8" ||val=="9" ||val=="0"){
+                setinputBoxval(val)
+                onDone()
+            }
+            else{
+
+            }
         }} type="text" className="m-1 w-[40px] h-[50px] rounded-xl 
         bg-blue-800 outline-0 px-4 text-white"></input>
     </div>
