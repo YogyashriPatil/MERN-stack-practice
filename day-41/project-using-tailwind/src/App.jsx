91139ee5e187ce66949a96d0dbd3b0999e6187c9
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-06 09:49:33 +0530

    Add the some extra css and change the sidebar

--- day-41/project-using-tailwind/src/App.jsx
+++ day-41/project-using-tailwind/src/App.jsx
@@ -1,11 +1,38 @@
-
 import { useEffect, useState } from 'react'
 import './App.css'
 import { Sidebar4 } from './components/sidebars/Sidebar4'
 import { SidebarToggle } from './icons/SidebarToggle'
+
+const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, query]);
+
+  return matches;
+};
 function App() {
+
   const [sidebarOpen , setSidebarOpen] = useState(true)
-   return (
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  console.error(isDesktop)
+  
+  useEffect(() => {
+    if (isDesktop == false) {
+      setSidebarOpen(false)
+    } else {
+      setSidebarOpen(true)
+    }
+  }, [isDesktop])
+  return (
     <div className='flex'>
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <MainContent sidebarOpen={sidebarOpen} />
