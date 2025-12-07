6e5f6aa3a14f481b90750975abbf23be2bc80632
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-07 17:15:38 +0530

    creating the function in advanced way

---
+++ day-42/fun.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    console.log("hello , " + name);
+}
+greet("Yogyashri");
+//# sourceMappingURL=fun.js.map
