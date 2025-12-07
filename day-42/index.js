93f0434d84f8576713febb38fcebb56c161ba4db
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-07 17:13:52 +0530

    create the function in typescript

--- day-42/index.js
+++ day-42/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    console.log(name + " welcome to typescript");
+}
+greet("hello");
 let x = 1;
 console.log(x + "number");
 //# sourceMappingURL=index.js.map
--- day-42/index.ts
+++ day-42/index.ts
@@ -1,2 +1,6 @@
+function greet(name: string){
+    console.log(name + " welcome to typescript")
+}
+greet("hello")
 let x =1 
 console.log(x + "number")
