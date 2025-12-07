1ca00d5997f2eb5b7d8b39b6498d0c48b287c2ef
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-07 17:50:57 +0530

    Add the gitigonre file

--- day-42/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    console.log(name + " welcome to typescript");
-}
-greet("hello");
-let x = 1;
-console.log(x + "number");
-//# sourceMappingURL=index.js.map
