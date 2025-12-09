728588f21c040146100982e1cd4321414dc4e034
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-09 12:52:23 +0530

    learn the function in typescript

--- day-43/src/function-interface.ts
+++ day-43/src/function-interface.ts
@@ -2,4 +2,11 @@ function greet(name: string): string{
     return "Hello " +name;
 }
 
+function isEven(num: number): boolean{
+    if(num%2 ==0 )
+        return true
+    else 
+        return false
+}
+console.log(isEven(4))
 console.log(greet("yogi"))
