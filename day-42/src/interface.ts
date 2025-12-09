825762285a7a961f5fe7a599048db87c870bc9d6
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-09 11:08:15 +0530

    learn about the interface

--- day-42/src/interface.ts
+++ day-42/src/interface.ts
@@ -1,1 +1,15 @@
+let firstname : string = "harkirat"
+let age: number = 19
 
+interface UserType {
+    firstname: string,
+    lastname: string,
+    age: number
+}
+
+let user : UserType= {
+    firstname:"yogyashri",
+    lastname:"patil",
+    age:21
+}
+console.log(user)
