f2a58259f66415d82aa84b54da4f33e98759ea1f
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-16 14:08:09 +0530

    Make the simple websocket server

--- day-45/src/index.ts
+++ day-45/src/index.ts
@@ -1,1 +1,14 @@
-console.log("hi there")
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({port: 8080})
+
+// event handler
+wss.on("connection", function(socket){
+    console.log("user connected")
+
+    setInterval(() => {
+        socket.send("hello"+Math.random());
+    },500)
+    
+
+})
