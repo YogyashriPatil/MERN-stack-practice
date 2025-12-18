23f1678445203160fc0a9ef36f62c6087b380037
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-18 20:51:03 +0530

    make the todo app using the postgresql and maked the signup route

--- day-47/src/index.ts
+++ day-47/src/index.ts
@@ -1,6 +1,10 @@
+import express from "express"
 import { configDotenv } from "dotenv";
 import { Client } from "pg";
 configDotenv();
+
+const app = express();
+app.use(express.json())
 const pgClient = new Client(process.env.postgres_db)
 
 // const pgClient2 = new Client({
@@ -10,9 +14,21 @@ const pgClient = new Client(process.env.postgres_db)
 //     host:"",
 //     database:""
 // })
-async function main() {
-    await pgClient.connect();
-    const response = await pgClient.query("SELECT * FROM users");
-    console.log(response);
-}
-main()
+pgClient.connect();
+
+app.post("/signup",async (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    const insertQuery =  `INSERT INTO users (username , email , password) values ('${username}','${email}','${password}');`
+    const response = await pgClient.query(insertQuery)
+    if(response){
+        res.json({
+            message:"you are signed up"
+        })
+    }
+})
+
+app.listen(3000, () => {
+    console.log("listening")
+})
