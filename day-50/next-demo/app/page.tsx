d7d1b8370999e0b3bef44e8ede510496f2696e01
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-21 18:37:06 +0530

    learn about the layout file

--- day-50/next-demo/app/page.tsx
+++ day-50/next-demo/app/page.tsx
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <div >
       hi there
+      <Button>Sign in</Button>
     </div>
   );
+}
+
+function Button({children}){
+  return <button>{children}</button>
 }
