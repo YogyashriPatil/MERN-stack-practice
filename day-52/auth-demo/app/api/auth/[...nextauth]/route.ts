2c5e6807d8c6732cf53e0ae691be794b6a6ba342
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-24 20:51:13 +0530

    Add the auth using the github and google

--- day-52/auth-demo/app/api/auth/[...nextauth]/route.ts
+++ day-52/auth-demo/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from 'next-auth/providers/github'
 const handler = NextAuth({
     providers:[
         CredentialsProvider({
@@ -26,6 +28,14 @@ const handler = NextAuth({
                     return null
                 }
             }
+        }),
+        GoogleProvider({
+            clientId:"Asd",
+            clientSecret:"adf"
+        }),
+        GitHubProvider({
+            clientId:"asdf",
+            clientSecret:"Asdf"
         })
     ]
 })
