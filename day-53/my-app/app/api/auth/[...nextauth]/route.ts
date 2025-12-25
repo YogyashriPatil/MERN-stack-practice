ab077b11c7df32323587887155d0167168a6c683
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-25 17:50:03 +0530

    Authenticated and unauthenticated feature added

---
+++ day-53/my-app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from 'next-auth/providers/github'
+const handler = NextAuth({
+    providers:[
+        CredentialsProvider({
+            name: "Login with email",
+            credentials:{
+                username: {label: "Username", type:"text", placeholder:'abc@gmail.com'},
+                password: {label:"Password", type:"password"}
+            },
+            async authorize(credentials, req){
+                const username = credentials?.username;
+                const password = credentials?.password;
+                // db req  to check if this username and password
+                console.log(username)
+                console.log(password)
+                const user = {
+                    name:"abc",
+                    id:1,
+                    username:"abc@gmail.com"
+                }
+                if(user){
+                    return user
+                }
+                else {
+                    return null
+                }
+            }
+        }),
+        GoogleProvider({
+            clientId:"Asd",
+            clientSecret:"adf"
+        }),
+        GitHubProvider({
+            clientId:"asdf",
+            clientSecret:"Asdf"
+        })
+    ]
+})
+export { handler as GET, handler as POST}
--- day-53/my-app/app/page.tsx
+++ day-53/my-app/app/page.tsx
@@ -1,9 +1,21 @@
+"use client"
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
+  return <div>
+    <SessionProvider>
+      <ReaHome />
+    </SessionProvider>
+  </div>
+}
+
+function ReaHome () {
+  const session = useSession()
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+        {session.status === "authenticated" ? <button onClick={() => signOut()}>Logout</button>:"error"}
+        {session.status === "unauthenticated" ? <button onClick={() => signIn()}>Sign in</button>:"error"}
     </div>
   );
 }
